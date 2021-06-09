c2ebf8ad43c7b08dcd556812cf2f3afb2dea9313
Ankur Atri <ankuratri.in@gmail.com>
2021-06-09 19:56:15 +0530

    generic implementation of Memoization problem added

--- memoizationProblem.js
+++ memoizationProblem.js
@@ -7,7 +7,7 @@
  */
 
 /**
- * Problem
+ * Problem 1
  *
  * Implement a function called memoizedSquare which will calculate the square
  * of an number for the first time and for the subsequent times,
@@ -43,3 +43,44 @@ console.log(memoSquare(4));
 console.log(memoSquare(2));
 console.log(memoSquare(3));
 console.log(memoSquare(4));
+
+/**
+ * Problem 2
+ *
+ * Implement a function called memoizedSum which will calculate the sum
+ * of arguments for the first time and for the subsequent times,
+ * will return from cache the already computed result
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+// Generic implementation of Memoization problem, this fn can be used for square, sum etc.
+function memoize(callback) {
+  const cache = {};
+
+  return function (...args) {
+    const key = args.toString();
+
+    if (key in cache) {
+      console.log(`Returning from cache for ${key}...`);
+      return cache[key];
+    } else {
+      console.log(`Computing result for ${key}...`);
+      const result = callback(...args);
+      cache[key] = result;
+      return result;
+    }
+  };
+}
+
+const memoSum = memoize(add);
+
+console.log(memoSum(1, 2));
+console.log(memoSum(2, 3));
+console.log(memoSum(4, 5));
+
+console.log(memoSum(1, 2));
+console.log(memoSum(2, 3));
+console.log(memoSum(4, 5));
